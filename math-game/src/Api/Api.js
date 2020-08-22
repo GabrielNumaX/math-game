f868d31a78a5894fe8b18a56f2c840e9f1e6700f
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github>
2020-08-22 02:00:43 -0300

    added api address for heroku

--- math-game/src/Api/Api.js
+++ math-game/src/Api/Api.js
@@ -1,8 +1,20 @@
+const protocol = window.location.protocol;
+const URLmaster = window.location.host;
+
+
+// const API = {
+
+//     'GET_POST_PLAYER': 'http://localhost:3030/mathtrix/hall',
+//     'GET_POINTS': 'http://localhost:3030/mathtrix/hall/position/',
+
+// }
+
 const API = {
 
-    'GET_POST_PLAYER': 'http://localhost:3030/mathtrix/hall',
-    'GET_POINTS': 'http://localhost:3030/mathtrix/hall/position/',
+    'GET_POST_PLAYER': `${protocol}//${URLmaster}/mathtrix/hall`,
+    'GET_POINTS': `${protocol}//${URLmaster}/mathtrix/hall/position/`,
 
 }
+
 
 export default API;
